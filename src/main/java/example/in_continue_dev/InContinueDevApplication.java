5e80c7c851ae324add1ceceda72c30ffff2b768d
eomheeseung <jeaimeshe@gamil.com>
2024-09-23 16:50:52 +0900

    first commit

---
+++ src/main/java/example/in_continue_dev/InContinueDevApplication.java
@@ -0,0 +1,13 @@
+package example.in_continue_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InContinueDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InContinueDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/example/in_continue_dev/InContinueDevApplicationTests.java
@@ -0,0 +1,13 @@
+package example.in_continue_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InContinueDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
