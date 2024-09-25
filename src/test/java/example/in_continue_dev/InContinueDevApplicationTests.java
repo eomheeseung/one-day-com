6412a50cf7a40e6952efc47d2802247c7c5de19d
eomheeseung <jeaimeshe@gamil.com>
2024-09-25 21:08:32 +0900

    09.25) github actions automation deploy

--- src/test/java/example/in_continue_dev/InContinueDevApplicationTests.java
+++ src/test/java/example/in_continue_dev/InContinueDevApplicationTests.java
@@ -3,7 +3,7 @@ package example.in_continue_dev;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = InContinueDevApplicationTests.class)
 class InContinueDevApplicationTests {
 
 	@Test
